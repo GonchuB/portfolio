4d7371e87c18699a22cd16b3fbc42dedc5b0c11b
Gonzalo Beviglia <gonchu.b@gmail.com>
2018-12-27 16:20:00 +0100

    Add og meta

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -5,12 +5,22 @@ import '../../assets/scss/main.scss';
 import Header from '../Header';
 import data from './data.json';
 
-const Layout = ({ children, data: { title, description } }) => (
+const Layout = ({
+  children,
+  data: { title, description, image, imageAlt, url, type },
+}) => (
   <>
     <Helmet>
       <html lang="en" />
       <title>{title}</title>
       <meta name="description" content={description} />
+
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:image" content={image} />
+      <meta name="og:image:alt" content={imageAlt} />
+      <meta name="og:url" content={url} />
+      <meta name="og:type" content={type} />
     </Helmet>
 
     <Header />
